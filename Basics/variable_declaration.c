0c0fa61e8212893786b0fb145d901dfa0b376b8c
Sriyut Singh <sriyutsingh5@gmail.com>
2026-09-10 20:45:08 +0530

    Enhance comments on variable declaration and data types
    Added comments explaining data types in C.

--- Basics/variable_declaration.c
+++ Basics/variable_declaration.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 int main(){
   int a; // DataType of the declared variable decision.
-
+  // There are 4 types of datatypes used mainly :- Integer(int),Decimal(float),String(char),Double{Used to store high precision data}(double)
   a=7;
 // Assigning the value of the variable.
   printf("%d",a);// Printing the varivale declared.
 
   return 0;
 }
+
+// diffrent data-types have diffrent functions
+
+// int:- it is used to store integers.
+// float:- used for storing of decimal values.
+// char:- used to store alphabets and letters.
+// double :- used for storing all kind of datatypes with high precision.
