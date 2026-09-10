d61c5f60a2d3cb0fbfd66df3301d4e352ffa548c
Sriyut Singh <sriyutsingh5@gmail.com>
2026-09-10 20:36:52 +0530

    Add variable declaration example in C

---
+++ Basics/variable_declaration.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int a; // DataType of the declared variable decision.
+
+  a=7;
+// Assigning the value of the variable.
+  printf("%d",a);// Printing the varivale declared.
+
+  return 0;
+}
