77dd40c55569d5dd3aeeebf4d4409534c3a1a275
Sriyut Singh <sriyutsingh5@gmail.com>
2026-09-10 19:34:43 +0530

    Create Basics/Printing.c with Hello World program
    Add a simple C program that prints 'Hello World'.

---
+++ Basics/Printing.c
@@ -0,0 +1,10 @@
+#include<stdio.h>   //Header file 
+
+int main(){//Main Function
+  
+  printf("Hello World");//Print statement
+  
+  return 0;//is mainly used to tell the operating system that the program finished successfully. 
+}
+
+//All the commands are ended with an ";" in C-language.
