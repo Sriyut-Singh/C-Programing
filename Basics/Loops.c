34cfe5ddde460376f7daddf03d479c035054ce60
Sriyut Singh <sriyutsingh5@gmail.com>
2026-09-12 00:45:09 +0530

    Add loop examples with explanations in Loops.c
    Corrected for loop, added while and do-while loops with explanations.

--- Basics/Loops.c
+++ Basics/Loops.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 int main(){
   int a;
-  for(a=1;a<6;a++){
+  //for loop is given below
+  for(a=1;a<6;a++){//Will print numbers from 1 till 5 ,, "a++" is used post-increment.
     printf("%d \n",a);
     }
+  //while loop given below
+  while(a<7,a++){//will print 2 forever because in while loop if the condition is true once then it does run the whole statement forever
+    printf("%d \n",a);
+    }
+  //Do-While loop given below
+  do {
+        printf("%d\n", i); // Code block to execute
+        i++;               // Increment (Update expression)
+    } while (i <= 5);      // Condition checked at the end
 }
