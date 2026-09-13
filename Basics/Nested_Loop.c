f96c67b49e9571c0d83dfdd91fd2875b1355bc8a
Sriyut Singh <sriyutsingh5@gmail.com>
2026-09-13 22:18:20 +0530

    Add nested loop example in Nested_Loop.c
    This program demonstrates the use of nested loops in C, printing the outer loop index and the inner loop index.

---
+++ Basics/Nested_Loop.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+  int i,j;
+  for(i=0;i<4;i++){
+    printf("%d \n",i);
+    for(j=0;j<5;j++){
+      printf("%d \n",j);}
+  }
+}
+
+//Output:-
+0 
+0 
+1 
+2 
+3 
+4 
+1 
+0 
+1 
+2 
+3 
+4 
+2 
+0 
+1 
+2 
+3 
+4 
+3 
+0 
+1 
+2 
+3 
+4 
