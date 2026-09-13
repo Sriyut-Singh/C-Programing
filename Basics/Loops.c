922e3e76ff4e32421398582442856923939d67c2
Sriyut Singh <sriyutsingh5@gmail.com>
2026-09-13 22:20:39 +0530

    Correct loop structures and comments in Loops.c
    Fixed issues in loop implementations and comments.

--- Basics/Loops.c
+++ Basics/Loops.c
@@ -5,10 +5,12 @@ int main(){
   for(a=1;a<6;a++){//Will print numbers from 1 till 5 ,, "a++" is used post-increment.
     printf("%d \n",a);
     }
+  
   //while loop given below
   while(a<7,a++){//will print 2 forever because in while loop if the condition is true once then it does run the whole statement forever
     printf("%d \n",a);
     }
+  
   //Do-While loop given below
   do {
         printf("%d\n", i); // Code block to execute
