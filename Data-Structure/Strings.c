d28b02985865ccc5d190f8af57efbfce26e3c20c
Sriyut Singh <sriyutsingh5@gmail.com>
2026-09-15 22:32:09 +0530

    Create Strings.c

---
+++ Data-Structure/Strings.c
@@ -0,0 +1,4 @@
+//Strings are used to store characetrs in c-langugae.
+//for string we have to firstly define the variabel as string.
+//the numbers of items that are to be recorded in the string should be priorly mentioned .
+
